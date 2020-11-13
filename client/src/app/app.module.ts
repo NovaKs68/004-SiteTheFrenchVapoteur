30fce60b175bb3dd5aeee4d62dc6244be2d68c08
tom.willemin <willemintom03@gmail.com>
2020-11-13 17:18:48 +0100

    Ajout de l'esthetique de l'header.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBlogComponent } from './components/header-blog/header-blog.component';
+import {RouterModule, Routes} from '@angular/router';
+import { BlogViewComponent } from './pages/blog-view/blog-view.component';
+import { ArticleCardComponent } from './components/article-card/article-card.component';
+
+const appRoutes: Routes = [
+  { path: '', component: BlogViewComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBlogComponent,
+    BlogViewComponent,
+    ArticleCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
