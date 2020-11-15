7a2a55db600f171e00383e9086da324f61728e7d
tom.willemin <willemintom03@gmail.com>
2020-11-15 13:05:07 +0100

    Formation de la BDD en prévision des fonctionnalités.

--- BDD/init-dev.d/1-BDD-TheFrenchVapoteur-tables.sql
+++ BDD/init-dev.d/1-BDD-TheFrenchVapoteur-tables.sql
@@ -1,16 +1,99 @@
-DROP DATABASE IF EXISTS theFrenchVapoteur;
-CREATE DATABASE IF NOT EXISTS theFrenchVapoteur;
-USE theFrenchVapoteur;
+DROP DATABASE IF EXISTS thefrenchvapoteur;
+CREATE DATABASE IF NOT EXISTS thefrenchvapoteur;
+USE thefrenchvapoteur;
 
-CREATE USER IF NOT EXISTS 'tomwillemin'@'%' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON theFrenchVapoteur . * TO 'rootSQL'@'%';
+CREATE USER IF NOT EXISTS 'rootSQL'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON thefrenchvapoteur . * TO 'rootSQL'@'%';
 
-----------------------Table Projects----------------------
-CREATE TABLE projects (
-        id_project SMALLINT NOT NULL AUTO_INCREMENT,
-        title_project VARCHAR(255) NOT NULL,
-        picture_project MEDIUMBLOB,
-        resume_project VARCHAR(255) NOT NULL,
-        PRIMARY KEY (id_project)
+
+----------------------Table users_roles----------------------
+CREATE TABLE users_roles (
+        id_role SMALLINT NOT NULL AUTO_INCREMENT,
+        name_role VARCHAR(255) NOT NULL,
+        permission SMALLINT NOT NULL,
+        PRIMARY KEY (id_role)
 )
 Engine = INNODB;
+
+----------------------Table users----------------------
+CREATE TABLE users (
+        id_user SMALLINT NOT NULL AUTO_INCREMENT,
+        forename VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        pseudo VARCHAR(255) NOT NULL,
+        mail VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        date_creation DATE NOT NULL,
+        id_role SMALLINT NOT NULL,
+        PRIMARY KEY (id_user),
+        UNIQUE KEY mail (mail),
+        CONSTRAINT fk_user_id_users
+            FOREIGN KEY (id_role)
+            REFERENCES users_roles(id_role)
+            ON DELETE CASCADE
+)
+Engine = INNODB;
+
+----------------------Table articles----------------------
+CREATE TABLE articles (
+        id_article SMALLINT NOT NULL AUTO_INCREMENT,
+        title_main VARCHAR(255) NOT NULL,
+        resume VARCHAR(255) NOT NULL,
+        date_creation DATE NOT NULL,
+        date_modification DATE,
+        PRIMARY KEY (id_article)
+)
+Engine = INNODB;
+
+----------------------Table articles_paragraphes----------------------
+CREATE TABLE articles_paragraphes (
+        id_paragraphe SMALLINT NOT NULL AUTO_INCREMENT,
+        id_article SMALLINT NOT NULL,
+        paragraphe TEXT NOT NULL,
+        PRIMARY KEY (id_paragraphe),
+        CONSTRAINT fk_id_paragraphes_id_article
+            FOREIGN KEY (id_article)
+            REFERENCES articles(id_article)
+)
+Engine = INNODB;
+
+----------------------Table articles_titles----------------------
+CREATE TABLE articles_titles (
+        id_title SMALLINT NOT NULL AUTO_INCREMENT,
+        id_article SMALLINT NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id_title),
+        CONSTRAINT fk_article_id_article
+            FOREIGN KEY (id_article)
+            REFERENCES articles(id_article)
+)
+Engine = INNODB;
+
+----------------------Table articles_plan----------------------
+
+----------------------Table tags----------------------
+CREATE TABLE tags (
+        id_tag SMALLINT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(100) NOT NULL,
+        color VARCHAR(50) NOT NULL,
+        PRIMARY KEY (id_tag)
+
+)
+Engine = INNODB;
+
+----------------------Table articles_tags----------------------
+CREATE TABLE articles_tags (
+        id_tag SMALLINT NOT NULL,
+        id_article SMALLINT NOT NULL,
+        PRIMARY KEY (id_article, id_tag),
+            CONSTRAINT fk_article_tags_articles
+                FOREIGN KEY (id_article)
+                REFERENCES articles(id_article),
+            CONSTRAINT fk_article_tags_tags
+                FOREIGN KEY (id_tag)
+                REFERENCES tags(id_tag)
+                ON DELETE CASCADE
+)
+Engine = INNODB;
+
+
--- BDD/init-dev.d/2-BDD-TheFrenchVapoteur-Data.sql
+++ BDD/init-dev.d/2-BDD-TheFrenchVapoteur-Data.sql
@@ -1,6 +1,1 @@
-use theFrenchVapoteur;
-
-INSERT INTO `projects` (`id_project`, `title_project`, `picture_project`, `resume_project`) VALUES
-(1,	'Robot Hexapod', '', 'Ce projet avait plusieurs buts. Le premier et le plus important était d\'apprendre. Le second était de découvrir ce domaine qui est la robotique. blabla..'),
-(2,	'Server NAS', '', 'L\'objectif de ce projet était de découvrir le monde des servers et d\'approfondir mes connaissances en linux. Au final il a été très enrichissant du point de vue de la gestion de linux. BlaBla...'),
-(3, 'Le site theFrenchVapoteur', '', 'Ce projet avait pour but d\'apprendre à initialiser des gros projets. C\'est pour cela que j\'ai utilisé beaucoup de grosses technologies (plus grosses que le site n\'aurait besoin.BALBALBKA)');
+use thefrenchvapoteur;
